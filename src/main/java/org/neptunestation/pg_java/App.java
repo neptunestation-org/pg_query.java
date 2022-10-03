53d169c7bfd364ea501914d345933eae85e8e7d9
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-10-03 13:45:30 -0700

    Simple formatting changes

--- src/main/java/org/neptunestation/pg_java/App.java
+++ src/main/java/org/neptunestation/pg_java/App.java
@@ -4,10 +4,8 @@ package org.neptunestation.pg_java;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
