b7c5b15df985d1ee77fdfb3c42228e796bbee3f3
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-10-05 08:33:08 -0700

    Got SWIG to work sort of

--- src/main/java/org/neptunestation/pg_java/App.java
+++ src/main/java/org/neptunestation/pg_java/App.java
@@ -5,7 +5,7 @@ package org.neptunestation.pg_java;
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+		public static void main(String[] args) {
+				System.out.println("Hello World!");
+		}
 }
