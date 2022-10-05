0a68ae0766138e60f342b7e69791036195d5bf3f
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-10-05 09:47:40 -0700

    Creating a package

--- src/main/java/org/neptunestation/pg_java/App.java
+++ src/main/java/org/neptunestation/pg_java/App.java
@@ -7,5 +7,6 @@ package org.neptunestation.pg_java;
 public class App {
 		public static void main(String[] args) {
 				System.out.println("Hello World!");
+				PgJava.pg_query_parse("select * from account");
 		}
 }
