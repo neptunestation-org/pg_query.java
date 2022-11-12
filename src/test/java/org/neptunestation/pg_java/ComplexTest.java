50bb8b2e1b710b49b9f9a5563085e415a62e15f2
NeptuneStation.org <neptunestationorg@david-desktop>
2022-11-12 13:28:14 -0800

    Added ComplexTest.java

---
+++ src/test/java/org/neptunestation/pg_java/ComplexTest.java
@@ -0,0 +1,20 @@
+package org.neptunestation.pg_java;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.*;
+import org.junit.*;
+
+public class ComplexTest {
+    @Test
+    public void main () throws IOException {
+	String fingerprint = "fc5a0500dddc25aa";
+	InputStream input = getClass().getResourceAsStream("/test.properties");
+	Properties prop = new Properties();
+	prop.load(input);
+	String query = prop.getProperty("query");
+	PgQueryFingerprintResult result = PgJava.pg_query_fingerprint(query);
+	assertEquals(result.getFingerprint_str(), fingerprint);
+    }
+}
